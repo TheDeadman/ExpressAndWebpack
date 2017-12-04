a305847439b5aeb2c3da17e5608eb957aa1154a2
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2017-12-04 00:07:23 -0800

    socket io-tized

--- front-end-src/js/app.js
+++ front-end-src/js/app.js
@@ -1,4 +1,5 @@
 import { reverseString } from './lib/utils'
+import io from 'socket.io-client'
 
 let s = "this is a string"
 
@@ -7,3 +8,23 @@ console.log(s)
 s = reverseString(s)
 
 console.log(s)
+
+// Create WebSocket connection.
+const socket = io()
+
+
+// Connection opened
+socket.on('open', function (event) {
+    console.log("opened")
+    socket.send('Hello Server!');
+});
+
+// Listen for messages
+socket.on('message', function (event) {
+    console.log("messaged")
+    console.log('Message from server ', event.data);
+});
+
+socket.on('news', function (event) {
+    console.log(event)
+})
